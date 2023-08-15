ff5d3393d70cf9e96ef928d28a2d98a822a05be5
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-15 15:18:39 +0530

    initial commit

---
+++ course-selling-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div
+    className=' flex gap-1 container p-4'
+    >
+      <h1 className=' text-xl'>Course selling app</h1>
+    </div>
+  )
+}
