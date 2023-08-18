389adfb62eee0478cb256bac5665090151e5e3f6
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-18 07:33:29 +0530

    USERS COURSES GET

---
+++ course-selling-app/src/app/api/users/courses/route.tsx
@@ -0,0 +1,28 @@
+import { Course, User } from "@/models";
+import { connectDb } from "@/helper/db";
+import { NextResponse } from "next/server";
+import { getResponseMessage } from "@/helper/error";
+
+connectDb();
+
+export async function GET(request:Request,response:NextResponse){
+    
+    try {
+        const courses = await Course.find();
+        return NextResponse.json(courses);
+        }
+
+     catch (error) {
+
+        return getResponseMessage("error in getting data !!" ,404,false);
+        // console.log(error);
+        // console.log("failed to get the courses from admin")
+        // return NextResponse.json({
+        //     message:"failed to get courses from admin",
+        //     success:false
+        // })
+        
+        
+    }
+       
+}
