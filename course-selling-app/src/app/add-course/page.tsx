22af4c609d79fc29a2a8eb766046354fa74f6b04
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-18 16:08:45 +0530

    new add course designs

--- course-selling-app/src/app/add-course/page.tsx
+++ course-selling-app/src/app/add-course/page.tsx
@@ -1,4 +1,7 @@
+"use client"
 import React from 'react'
+import loginSvg from "../assets/login.svg";
+import Image from 'next/image';
 
 export const metadata={
     title:"Add Course: JamStore",
@@ -12,21 +15,89 @@ const AddCourse = () => {
         shadow-orange-200 shadow 
         p-5
         '>
-            <h1 className='text-3xl text-white'> Add your course</h1>
+            <div className='my-8 flex justify-center items-center'>
+                <Image alt='image' 
+                style={{
+                 width:"50%",
+                }}
+                src={loginSvg}/>
+            </div>
+            <h1 className='text-3xl text-center '> Add your course</h1>
             <form action="#!">
+                {/* title */}
                 <div className=' mt-4'>
-                    <label htmlFor="title">Title</label>
+                    <label htmlFor="title" 
+                    className='  block text-sm mb-2'
+                    >Title</label>
                     <input  type="text"
-                     className=' block text-sm text-white
-                     font-medium mb-2 w-full p-3 rounded-3xl
+                     className=' 
+                     font-medium  w-full p-3 rounded-3xl
                      bg-slate-500 focus:ring-gray-100
                      border border-gray-800  
                      
                      '
                      id='course_title'
                      />
-
                 </div>
+                {/* description */}
+                <div className=' mt-4'>
+                    <label htmlFor="description" 
+                    className='  block text-sm mb-2'
+                    >Description</label>
+                    <textarea  
+                     className=' 
+                     font-medium  w-full p-3 rounded-3xl
+                     bg-slate-500 focus:ring-gray-100
+                     border border-gray-800  
+                     
+                     '
+                     id='course_description'
+                     rows={3}
+                     />
+                </div>
+
+                {/* price */}
+
+                <div className=' mt-4'>
+                    <label htmlFor="price" 
+                    className='  block text-sm mb-2'
+                    >Price</label>
+                    <input  type="number"
+                     className=' 
+                     font-medium  w-full p-3 rounded-3xl
+                     bg-slate-500 focus:ring-gray-100
+                     border border-gray-800  
+                     
+                     '
+                     id='course_price'
+                     />
+                </div>
+
+                {/* image */}
+
+                <div className=' mt-4'>
+                    <label htmlFor="imageLink" 
+                    className='  block text-sm mb-2'
+                    >ImageLink</label>
+                    <input  type="url"
+                     className=' 
+                     font-medium  w-full p-3 rounded-3xl
+                     bg-slate-500 focus:ring-gray-100
+                     border border-gray-800  
+                     
+                     '
+                     id='course_imageLink'
+                     />
+                </div>
+
+                {/* button */}
+                <div className='flex justify-center gap-x-4 mt-4'>
+                    <button className=' bg-blue-700 p-3 rounded-full
+                     hover:bg-blue-800 '>Add Course</button>
+                     <button className=' bg-red-700 p-3 rounded-full
+                     hover:bg-red-800 '>Clear</button>
+                </div>
+                
             </form>
 
         </div>
--- course-selling-app/src/components/Footer.tsx
+++ course-selling-app/src/components/Footer.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 const Footer = () => {
-  return (<footer className='h-40 bg-white mt-5'>
+  return (<footer className='h-40 bg-slate-800 mt-5'>
     <div className=' flex p-5 justify-around'>
       <div className=' text-center flex flex-col justify-center '>
           <h1 className=' text-3xl '>Welcome to JamStore</h1>
