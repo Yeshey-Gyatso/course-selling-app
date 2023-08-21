06150d5e46a1eaac53a53f80d779dd1b3bf893db
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-21 12:00:27 +0530

    middleware added first test

--- course-selling-app/src/middleware.ts
+++ course-selling-app/src/middleware.ts
@@ -3,10 +3,11 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+    console.log("middleware executed")
+//   return NextResponse.redirect(new URL('/home', request.url))
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/about/:path*',
+  matcher: '/add-course',
 }
