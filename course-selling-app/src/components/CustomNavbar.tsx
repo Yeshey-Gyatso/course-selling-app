da7e325fb8e731ab3200d696eacf4df8ef316907
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-21 19:29:12 +0530

    added logout and usecontext

--- course-selling-app/src/components/CustomNavbar.tsx
+++ course-selling-app/src/components/CustomNavbar.tsx
@@ -1,10 +1,27 @@
-"use client"
+"use client";
 
 
+import UserContext from '@/context/userContext';
+import { logout } from '@/services/userService';
 import Link from 'next/link';
-import React from 'react'
+import React, { useContext } from 'react'
+import { toast } from 'react-toastify';
 
 const CustomNavbar = () => {
+  const context=useContext(UserContext);
+  
+  async function doLogout(){
+      try { 
+          const result=await logout();
+          context.setUser(undefined); 
+      } catch (error) {
+        console.log(error)
+        toast.error("Logout error")
+        
+      }
+  }
+
+
   return (
     <div className=' flex md:justify-between 
      bg-gray-500
@@ -16,17 +33,54 @@ const CustomNavbar = () => {
         </div>
         <div className=' flex space-x-2  '>
             <h1><Link href={'/'} className=' hover:text-gray-300'>Home</Link></h1>
+            {
+              context.user &&(
+                <>
             <h1><Link href={'/courses' }className=' hover:text-gray-300'>Courses</Link></h1>
+                
+                </>
+
+              )
+            }
             </div>
-        <div><ul className='flex   space-x-2 '>
+        <div>
+              {
+                context.user && (
+                  <>
+                  <ul className='flex   space-x-2 '>
                 <li>
-               <Link href={'/login'}>login</Link>
+               <Link href={'/login'} 
+                onClick={doLogout}
+               >login</Link>
                 </li>
                 <li>
                   <Link href={'/signupUser'}>Signup</Link>
                 
                 </li>
-            </ul></div>
+            </ul>
+                  </>
+                )
+              }
+              {
+                !context.user && (
+                  <>  
+                    <ul className='flex   space-x-2 '>
+                <li>
+               <Link href={'#!'} 
+               >{context.user.name}</Link>
+                </li>
+                <li>
+                  <Link href={'/signupUser'}
+                   onClick={doLogout}
+                  >Log out</Link>
+                
+                </li>
+            </ul>
+                  </>
+                )
+              }
+
+        </div>
         </div>
   )
 }
