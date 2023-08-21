0a6182d0c64f10dc75577f83d57eccbd78442867
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-21 16:14:05 +0530

    userContext created

---
+++ course-selling-app/src/app/api/current/route.tsx
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import { User } from "@/models";
+
+export async function GET(request:NextResponse){
+    const authToken=request.cookies.get("authToken")?.value;
+    // console.log(authToken);
+    const payload=jwt.verify(authToken, process.env.JWT_KEY);
+    // console.log(payload);
+
+    const user=await User.findById(payload._id).select("-password")
+
+    return NextResponse.json(user)
+}
