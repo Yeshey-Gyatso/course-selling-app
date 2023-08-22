39620554d9d45fb387147bfebd20752336618442
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-22 17:22:08 +0530

    debugging for db connection

--- course-selling-app/src/app/api/users/[userId]/purchasedCourses/route.tsx
+++ course-selling-app/src/app/api/users/[userId]/purchasedCourses/route.tsx
@@ -2,9 +2,10 @@ import { Course, User } from "@/models";
 import { connectDb } from "@/helper/db";
 import { NextResponse } from "next/server";
 import { getResponseMessage } from "@/helper/error";
-connectDb();
+
 export async function GET(request:Request,{ params }){
     const {userId}=params;
+    await connectDb();
     const user=await User.findById(userId).populate('purchasedCourses');
     if (user) {
         return NextResponse.json({ purchasedCourses: user.purchasedCourses || [] });
