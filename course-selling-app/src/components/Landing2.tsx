dab6f79f7244a21a2051257eed681f98daf3c46b
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-22 19:14:18 +0530

    final landing update

--- course-selling-app/src/components/Landing2.tsx
+++ course-selling-app/src/components/Landing2.tsx
@@ -5,12 +5,10 @@ import {GiMailbox} from 'react-icons/gi';
 import { motion as m } from "framer-motion"
 import Link from 'next/link';
 const Landing2 = () => {
-            
   return (
     <section  
      //  bg-black/50
     className='snap-y snap-mandatory h-screen w-full
-   
      flex items-center
      bg1
      bg-cover
@@ -28,7 +26,7 @@ const Landing2 = () => {
         '>
             
               <div 
-                className='relative z-10 flex flex-col items-center'>
+                className='relative z-1 flex flex-col items-center'>
                 <div className='relative flex flex-row items-center'>
                 <m.h2 
                 initial={{opacity:0,y:"100%"}} 
@@ -36,7 +34,7 @@ const Landing2 = () => {
                 transition={{duration:1 , ease:"easeInOut"}}
 
                 animate={{y:-60,opacity:1}}
-                className='cursor-default text-2xl sm:text-4xl font-bold md:6xl text-white'>
+                className='absolute right-96 cursor-default text-2xl sm:text-4xl font-bold md:text-6xl text-black'>
                     WELCOME TO VINTAGE MELODIES
                 </m.h2>     
                   {/* <m.h2
@@ -46,7 +44,7 @@ const Landing2 = () => {
                      className='text-4xl text-opacity-0 md:text-opacity-100 font-bold  text-white pl-2'  
                     >!</m.h2>  */}
                 </div>      
-                     <div className=' my-10  py-10  bg-orange-700 opacity-70 w-full px-8 flex flex-col items-center '>
+                     <div className=' my-10  py-10 lg:ml-96 bg-orange-700 opacity-90 w-full px-8 flex flex-col items-center '>
                         <p className=' cursor-default py-4 text-white md:text-2xl max-w-full sm:mx-auto z-10 font-light   
                         '>
                             <span className=' font-quote'>"Music is the closest thing we have to magic."  </span><span className='  font-mono font-bold'>– Jim Carrey</span>
@@ -54,7 +52,7 @@ const Landing2 = () => {
                      </div>
                      
                      {/* smooth duration={800} */}
-                      <div className=' mt-auto'>
+                      <div className='  absolute right-0 bottom-0'>
                     <Link href='#!' className='group text-white w-fit px-6 py-3 my-2 flex 
                      items-center rounded-md
                      bg-gradient-to-r from-gray-600 to to-orange-200
