67fd89d7a76416ca6f5037616264824c91a1dcc6
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2023-08-23 17:37:55 +0530

    continuing purchase !

--- course-selling-app/src/app/courses/courses.tsx
+++ course-selling-app/src/app/courses/courses.tsx
@@ -19,16 +19,36 @@ const Courses = () => {
   useEffect(()=>{
       load()
   },[])
+
+  const Purchase=async()=>{
+
+    try {
+      const result= await addCourse(course)
+    } catch (error) {
+      
+    }
+    
+  }
+
+
   return (
 
     <div  >
-      <div className=' '>
-      <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+      <div className=' lg:pb-36 pb-20 '>
+      <div className="p-10  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         { course.map((task)=>(
             
+           
+            <div className='max-w-sm mt-10 
+            rounded overflow-hidden shadow-lg 
+            bg-white
             
-            <div className='max-w-sm mt-10 rounded overflow-hidden shadow-lg bg-white'>
+            '
+            key={task._id}
+            >
+            
             <img
+
               className="w-full"
               src={task.imageLink}
               alt="card"
@@ -36,20 +56,24 @@ const Courses = () => {
             />
       
             <div className="px-6 py-4">
-            
-              <p className="text-gray-700 text-base">
-              <div className="font-bold text-xl mb-2">
+            <div className="font-bold text-black text-xl mb-2">
               {task.title}
               </div>
+              <p className="text-gray-700 text-base">
+              
                {task.description}
               </p>
             </div>
             <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Instagram</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Facebook</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#YouTube</span>
             </div>
-            
+            <div className='px-6 pt-4 pb-2'>
+                <button onClick={Purchase} className='inline-block bg-blue-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2' >
+                  Purchase
+                </button>
+            </div>
             
             
             </div>
